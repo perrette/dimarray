47a13434f44080aac5a561d8d274d25df6225615
perrette <mahe.perrette@pik-potsdam.de>
2013-12-04 21:10:03 +0100

    fixed README

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open('README') as file:
         long_description = file.read()
 
 setup(name='laxarray',
-      version='1.2',
+      version='1.2.1',
       author='Mahe Perrette',
       author_email='mahe.perrette@pik-potsdam.de',
       description='Labelled Axis Array',
-      keywords=('labelled array','numpy','larry','pandas')
+      keywords=('labelled array','numpy','larry','pandas'),
       py_modules=['laxarray'],
       long_description=long_description,
       url='https://github.com/perrette/laxarray',
       license = "BSD 2-Clause",
-      classifier = [
+      classifiers = [
 	  "Development Status :: 3 - Alpha"
 	  ]
       )
