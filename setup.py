0e558ea4db0f8cc428b8c5101de11dff549e4ef8
perrette <mahe.perrette@pik-potsdam.de>
2013-12-05 14:01:05 +0100

    README updated

--- setup.py
+++ setup.py
@@ -3,17 +3,14 @@ from distutils.core import setup
 with open('README') as file:
         long_description = file.read()
 
-setup(name='laxarray',
+setup(name='dimarray',
       version='1.2.1',
       author='Mahe Perrette',
       author_email='mahe.perrette@pik-potsdam.de',
       description='Labelled Axis Array',
-      keywords=('labelled array','numpy','larry','pandas'),
-      py_modules=['laxarray'],
+      keywords=('labelled array','numpy','larry','pandas','iris'),
+      py_modules=['dimarray'],
       long_description=long_description,
-      url='https://github.com/perrette/laxarray',
+      url='https://github.com/perrette/dimarray',
       license = "BSD 2-Clause",
-      classifiers = [
-	  "Development Status :: 3 - Alpha"
-	  ]
       )
