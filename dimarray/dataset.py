8b4ea4cc2f020ceef27fd9973f8e8c8af345e197
perrette <mahe.perrette@pik-potsdam.de>
2014-01-15 19:36:23 +0100

    can now initialize Dataset with axis instance only

--- dimarray/dataset.py
+++ dimarray/dataset.py
@@ -7,66 +7,64 @@ from core import DimArray, array, Axis, Axes
 from core import align_axes 
 from core import pandas_obj
 
+def _get_list_arrays(data, keys):
+    """ initialize from DimArray objects (internal method)
+    """
+    if not isinstance(data, dict) and not isinstance(data, list):
+	raise TypeError("Type not understood. Expected list or dict, got {}: {}".format(type(data), data))
+
+    assert keys is None or len(keys) == len(data), "keys do not match with data length !"
+
+    # Transform data to a list
+    if isinstance(data, dict):
+	if keys is None:
+	    keys = data.keys()
+	data = [data[k] for k in keys]
+    
+    # Check everything is a DimArray
+    for v in data:
+	if not isinstance(v, DimArray):
+	    raise TypeError("A Dataset can only store DimArray instances")
+
+    # Check names
+    for i, v in enumerate(data):
+	if not hasattr(v,'name') or not v.name:
+	    if keys is not None:
+		v.name = keys[i]
+	    else:
+		v.name = "v%i"%(i)
+
+    return data
+
 class Dataset(object):
     """ Container for a set of aligned objects
     """
-    def __init__(self, data, keys=None):
+    def __init__(self, data=None, keys=None, axes=None):
 	""" initialize a dataset from a set of objects of varying dimensions
 
-	data  : dict of DimArrays or list of named DimArrays
+	data  : dict of DimArrays or list of named DimArrays or Axes object
 	keys  : keys to order data if provided as dict, or to name data if list
 	"""
-	# Initialize an Axes object
-	self.axes = Axes()
+	assert data is None or axes is None, "can't provide both data and axes"
+
+	# Basic initialization
+	if axes is None:
+	    axes = Axes()
+	self.axes = axes
 	self.variables = odict()
 
-	assert keys is None or len(keys) == len(data), "key does not match with model length !"
+	# If initialized from data
+	if data is not None:
 
-	# Transform data to a list
-	if isinstance(data, dict): 
-	    if keys is None:
-		keys = data.keys()
-	    data = [data[k] for k in keys]
+	    # First, get a list of named DataFrames
+	    data = _get_list_arrays(data, keys)
 
-	# Check everything is a DimArray
-	for v in data:
-	    if not isinstance(v, DimArray):
-		raise TypeError("A Dataset can only store DimArray instances")
+	    # Align objects
+	    data = align_axes(*data)
 
-	# Check names
-	for i, v in enumerate(data):
-	    if not hasattr(v,'name') or not v.name:
-	#	raise ValueError("DimArray must have a name if provided as list, provide `keys=` parameter")
-		if keys is not None:
-		    v.name = keys[i]
-		else:
-		    v.name = "v%i"%(i)
-
-	# Align objects
-	data = align_axes(*data)
-
-	# Append to dictionary
-	for v in data:
-	    self[v.name] = v
-
-#    @classmethod
-#    def from_dict(cls, dict_, keys=()):
-#	""" initialize from dictionary
-#	"""
-#	# use keys to order the dictionary
-#	args = []
-#	for k in keys:
-#	    data = dict_.pop(k)
-#	    data.name = k
-#	    args.append(data)
-#
-#	return cls(*args, **dict_)
-#     
-#    @classmethod
-#    def from_arrays(cls, ):
-#	""" initialize from dictionary
-#	"""
-#	pass
+	    # Append object (will automatically update self.axes)
+	    for v in data:
+		self[v.name] = v
 
     @property
     def dimensions(self):
