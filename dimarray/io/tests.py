16dda1f403965d2eaa6ff1fa0309aca6d9022c6d
perrette <mahe.perrette@pik-potsdam.de>
2014-02-02 23:46:22 +0100

    added netCDF I/O test

--- dimarray/io/tests.py
+++ dimarray/io/tests.py
@@ -2,24 +2,40 @@
 """
 import os
 
-import dimarray 
+import numpy as np
+
+import dimarray  as da
 from dimarray.testing import testmod
-import nc
+from dimarray import DimArray, summary_nc, read_nc
 
 curdir = os.path.dirname(__file__)
 testdata = os.path.join(curdir, 'testdata')
 
-def _main(**kwargs):
-    """ go to testdata and make sure the "test.nc" file exist
-    """
-    curdir = os.path.abspath(os.getcwd()) # current directory
-    os.chdir(testdata) # change to testdata
-    res = testmod(nc, **kwargs)
-    os.chdir(curdir) # come back to current directory
-    return res
+def test_ncio():
+    a = DimArray([1,2], dims='xx0')
+    b = DimArray([3,4,5], dims='xx1')
+    fname = os.path.join(testdata, "test.nc")
+    a.write_nc(fname,"a", mode='w')
+    b.write_nc(fname,"b", mode='a')
+    data = read_nc(fname)
+    assert(np.all(data['a'] == a))
+    assert(np.all(data['b'] == b))
+    ds = da.Dataset([a,b],keys=['a','b'])
+    for k in ds:
+	assert(np.all(ds[k] == data[k]))
+
+
+#def _main(**kwargs):
+#    """ go to testdata and make sure the "test.nc" file exist
+#    """
+#    curdir = os.path.abspath(os.getcwd()) # current directory
+#    os.chdir(testdata) # change to testdata
+#    res = testmod(nc, **kwargs)
+#    os.chdir(curdir) # come back to current directory
+#    return res
 
 def main(**kw):
-    pass
+    test_ncio()
 
 if __name__ == "__main__":
     main()
