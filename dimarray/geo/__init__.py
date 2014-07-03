6441ab0c741bac90c11f8dc7d3a7dbc40b7e5c86
perrette <mahe.perrette@pik-potsdam.de>
2014-07-03 12:59:18 +0200

    cleaned up geo array : removed all useless stuff. py.test fails: needs check

--- dimarray/geo/__init__.py
+++ dimarray/geo/__init__.py
@@ -1,7 +1,11 @@
 """ This module is experimental, and will likely undergo many changes
 """
+# import all basic DimArray-like objects
+# so that one can also do: import dimarray.geo as da
+from dimarray import *
 
-from ..core import *
+# Add GeoArray to the list
 from geoarray import GeoArray
-from transform import *
+
+# Overwrite with specific GeoArray functions
 from io import *
