5aef31bdcae532ce880ca2b51c128c7d8a2289a1
perrette <mahe.perrette@pik-potsdam.de>
2014-07-05 21:10:56 +0200

    dimarray/info.py : error message with general info about dimarray

---
+++ dimarray/info.py
@@ -0,0 +1,6 @@
+github="github.com/perrette/dimarray"
+github_issues="github.com/perrette/dimarray/issues"
+
+def file_an_issue_message():
+    return "Please report any problem or feature request at {}".format(github_issues)
+
