a6225053c23c4311937bb81323c9a1e2dad0e0c2
Mahé Perrette <mahe.perrette@gmail.com>
2014-08-29 18:12:31 +0200

    fix __version__ bug

--- dimarray/__init__.py
+++ dimarray/__init__.py
@@ -14,7 +14,11 @@ from datasets import get_datadir, get_ncfile
 #from lib.transform import interp1d, interp2d, apply_recursive
 from lib import *
 
-from dimarray.version import version as __version__
+# need to fix that
+try:
+    from dimarray.version import version as __version__
+except:
+    pass
 
 try:
     from io.nc import read_nc, summary_nc, write_nc #, read_nc_axes
